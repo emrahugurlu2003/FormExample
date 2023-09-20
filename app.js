99ca5f41432263d5dbd50acdbeeb2930a2d912df
emrahugurlu2003 <135663130+emrahugurlu2003@users.noreply.github.com>
2023-09-20 13:19:03 +0300

    user selection appended

--- app.js
+++ app.js
@@ -19,9 +19,45 @@ function toggleModal() {
 }
 
 //* ------ Selectors ------- */
+const selectionArticle = document.querySelector(".selection");
+
+// //? Images
+// const rockImg = document.getElementById("rock");
+// const paperImg = document.getElementById("paper");
+// const scissorImg = document.getElementById("scissor");
+
+//? divs of the selected elements
+const yourChoiceDiv = document.getElementById("your-choice");
 
 //* ------- Variables ------- */
+// let image = document.createElement("img");
+let userSelectImg = document.createElement("img");
 
 //* ------- Event Listeners ------- */
+selectionArticle.addEventListener("click", (e) => {
+  // console.log(e.target.id)
+  if (e.target.id) {
+    userSelectImg.src = `./assets/${e.target.id}.png`;
+    userSelectImg.alt = e.target.id;
+    yourChoiceDiv.appendChild(userSelectImg);
+  }
+});
+// rockImg.addEventListener("click", () => {
+//   image.src = "./assets/rock.png";
+//   image.alt = "rock";
+//   yourChoiceDiv.appendChild(image);
 
+//   //? innerHTML
+//   // yourChoiceDiv.innerHTML = `<img src="./assets/rock.png" alt="rock">`
+// });
+// paperImg.addEventListener("click", () => {
+//   image.src = "./assets/paper.png";
+//   image.alt = "paper";
+//   yourChoiceDiv.appendChild(image);
+// });
+// scissorImg.addEventListener("click", () => {
+//   image.src = "./assets/scissor.png";
+//   image.alt = "scissor";
+//   yourChoiceDiv.appendChild(image);
+// });
 //* ------- Functions ------- */
